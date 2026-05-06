6e64f93a64aef6d4d1ac2fc6874871a41b99ad30
Liam Bartie <liambartie@Liams-MacBook-Pro.local>
2026-05-06 10:31:09 -0700

    Hero overlay: classic black gradient. Tighter page header. More body padding.

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'WELLSPRING_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'WELLSPRING_VERSION', '0.7.1' );
+	define( 'WELLSPRING_VERSION', '0.7.2' );
 }
 
 /**
